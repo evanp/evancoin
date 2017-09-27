352c54a594a3ee15c853e04c3efc12157e35631a
Evan Prodromou <evan@fuzzy.ai>
2017-09-27 09:35:21 -0400

    Clear dir

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var EvanCoin = artifacts.require("./EvanCoin.sol");
-var EvanCoinCrowdsale = artifacts.require("./EvanCoinCrowdsale.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(EvanCoin);
-  deployer.deploy(EvanCoinCrowdsale);
-};
