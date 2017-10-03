a62fcdfd435197528b12487987a638b0befc292c
Evan Prodromou <evan@fuzzy.ai>
2017-10-03 15:04:22 -0400

    Remove all the market cruft

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 var EvanCoin = artifacts.require("./EvanCoin.sol");
-var EvanCoinMarket = artifacts.require("./EvanCoinMarket.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(EvanCoin);
-  deployer.deploy(EvanCoinMarket, EvanCoin.address);
-  web3.eth.
 };
